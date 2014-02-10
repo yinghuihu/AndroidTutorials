424d153f50d59d0c0186175fa50860b1130071be
yinghuihu <yinghuihu@gmail.com>
2014-02-10 08:59:59 -0800

    Initial work for bible plan handling

---
+++ 18Bible/src/com/chccc/bible/plan/ReadPlanInterface.java
@@ -0,0 +1,10 @@
+package com.chccc.bible.plan;
+
+import java.util.ArrayList;
+
+
+public interface ReadPlanInterface {
+
+	//Date format should be MM/dd/YYYY
+	public ArrayList getChaptersByDate(String date); 
+}
