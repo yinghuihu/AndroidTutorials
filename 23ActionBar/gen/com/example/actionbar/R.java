aa2fe3cec99f32a4f1404e5b83abdc268c0d68e3
yinghuihu <yinghuihu@gmail.com>
2014-02-25 08:09:41 -0800

    make it using search widget to search in action bar.

--- 23ActionBar/gen/com/example/actionbar/R.java
+++ 23ActionBar/gen/com/example/actionbar/R.java
@@ -17,43 +17,49 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_action_help=0x7f020000;
-        public static final int ic_action_location_found=0x7f020001;
-        public static final int ic_action_refresh=0x7f020002;
-        public static final int ic_action_search=0x7f020003;
-        public static final int ic_action_share=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int ic_action_android=0x7f020000;
+        public static final int ic_action_help=0x7f020001;
+        public static final int ic_action_location_found=0x7f020002;
+        public static final int ic_action_refresh=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_action_share=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_check_updates=0x7f080005;
-        public static final int action_help=0x7f080004;
-        public static final int action_location_found=0x7f080002;
-        public static final int action_refresh=0x7f080003;
-        public static final int action_search=0x7f080001;
-        public static final int action_settings=0x7f080000;
+        public static final int action_check_updates=0x7f090006;
+        public static final int action_help=0x7f090005;
+        public static final int action_location_found=0x7f090003;
+        public static final int action_refresh=0x7f090004;
+        public static final int action_search=0x7f090002;
+        public static final int action_settings=0x7f090001;
+        public static final int txtQuery=0x7f090000;
     }
     public static final class layout {
         public static final int activity_location_found=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_search_results=0x7f030002;
     }
     public static final class menu {
-        public static final int location_found=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int location_found=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int search_results=0x7f080002;
     }
     public static final class string {
-        public static final int action_check_updates=0x7f050007;
-        public static final int action_help=0x7f050006;
-        public static final int action_location_found=0x7f050004;
-        public static final int action_refresh=0x7f050005;
-        public static final int action_search=0x7f050003;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_location_found=0x7f050008;
+        public static final int action_check_updates=0x7f060008;
+        public static final int action_help=0x7f060007;
+        public static final int action_location_found=0x7f060005;
+        public static final int action_refresh=0x7f060006;
+        public static final int action_search=0x7f060004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060003;
+        public static final int search_hint=0x7f060002;
+        public static final int title_activity_location_found=0x7f060009;
+        public static final int title_activity_search_results=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -77,10 +83,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int searchable=0x7f040000;
     }
 }
--- 23ActionBar/src/com/example/actionbar/MainActivity.java
+++ 23ActionBar/src/com/example/actionbar/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.actionbar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
@@ -12,13 +15,23 @@ public class MainActivity extends Activity {
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_main);
+	        getActionBar().setIcon(R.drawable.ic_action_android);
 	    }
 	 
 	    @Override
 	    public boolean onCreateOptionsMenu(Menu menu) {
 	        getMenuInflater().inflate(R.menu.main, menu);
 	 
+	        
+	        // Associate searchable configuration with the SearchView
+	        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+	                .getActionView();
+	        searchView.setSearchableInfo(searchManager
+	                .getSearchableInfo(getComponentName()));
+	 
 	        return super.onCreateOptionsMenu(menu);
+	        
 	    }
 	    
 	    /**
